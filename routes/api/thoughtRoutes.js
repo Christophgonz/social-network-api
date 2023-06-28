573df4d7330b46f150bd947560e403ff497ba4df
cgonz <christoph.gonz@gmail.com>
2023-06-28 02:46:22 -0700

    added starter routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+router.route("/").get().post();
+router.route("/:thoughtId").get().put().delete();
+router.route("/:thoughtId/reactions").post();
+router.route("/:thoughtId/reactions/:reactionId").delete();
